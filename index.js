6e2e829f55d8b2167d40e50a74c372421344c42f
Alexander Buysse <alexanderbuysse@MacBook-Pro-van-Alexander-2.local>
2021-11-15 15:26:02 +0100

    chat socket werkt

--- index.js
+++ index.js
@@ -14,11 +14,9 @@ io.on('connection', (socket) => {
   socket.on('disconnect', () => {
     console.log('user disconnected');
   });
-  io.on('connection', (socket) => {
     socket.on('chat message', (msg) => {
       io.emit('chat message', msg);
     });
-  });
 });
 
 server.listen(3000, () => {
